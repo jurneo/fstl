54d8e768c1717346e69c5e642870a216fd9a8e3f
kusuma <kusuma@sunnyoptical.com>
2017-11-15 09:30:13 +0800

    change surface format

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <QApplication>
 
 #include "app.h"
-
+#include <QSurfaceFormat>
 int main(int argc, char *argv[])
 {
     App a(argc, argv);
+    QSurfaceFormat format;
+    format.setDepthBufferSize(24);
+    format.setVersion(4, 3);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
+
     return a.exec();
 }
